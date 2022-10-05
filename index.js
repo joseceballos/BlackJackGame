e8f0079339e769af9b689a4e02daa361a85747f7
joancele <joseceballos.developer@gmail.com>
2022-10-05 03:39:06 +0200

    lesson 3

--- index.js
+++ index.js
@@ -1,34 +1,96 @@
-let firstCard = Math.floor(Math.random() * 10) + 1
-let secondCard = Math.floor(Math.random() * 10) + 1
-
-let sum = firstCard + secondCard
+let cards = Array()
+let sum = 0
 
 let hasBlackJack = false
 let isAlive = true
 
+let cardsP = document.getElementById("cards-el")
+let sumP = document.getElementById("sum-el")
+let messageP = document.getElementById("message-el")
+let newCardButton = document.getElementById("buttonNewCard")
 let message = ""
+let cardsMessage = ""
 
-console.log(sum)
-
-function checkBlackJack ()
+function generateCard ()
 {
-    if (sum <= 20)
-    {
+    return (Math.floor(Math.random() * 10) + 2)
+}
 
-    }
+function addCard ()
+{
+    cards.push(generateCard())
+    updateGame ()
+}
 
-    else if (sum === 21)
+function disableAddCardButton (disable)
+{
+    newCardButton.disabled = disable
+
+    if(disable)
     {
-        hasBlackJack = true
+        newCardButton.style.backgroundColor = "grey"
     }
 
     else
     {
-        isAlive = false
+        newCardButton.style.backgroundColor = "goldenrod"
     }
+}
+
+function updateSum (card)
+{
+    sum += card
+}
+
+function printCards (card)
+{
+    cardsMessage += card + " "
+}
+
+function checkBlackJack ()
+{
+    cardsMessage = ""
+    sum = 0
+    cards.forEach(printCards);
+    cards.forEach(updateSum);
+
+    if (sum <= 20)
+    {
+        message = "Do you want to draw a new card?"
+    }
+
+    else if (sum === 21)
+    {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+        disableAddCardButton(true)
+    }
+
+    else
+    {
+        message = "You're out of the game!"
+        isAlive = false
+        disableAddCardButton(true)
+    }
+}
+
+function updateUI ()
+{
+    messageP.textContent = message
+    cardsP.textContent = "Cards: " + cardsMessage
+    sumP.textContent = "Sum: " + sum
+}
+
+function updateGame ()
+{
+    checkBlackJack()
+    updateUI()
 }
 
 function startGame() 
 {
-    console.log(1)
+    cards = Array()
+    addCard()
+    updateGame ()
+    disableAddCardButton(false)    
 }
