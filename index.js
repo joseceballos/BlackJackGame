aa4ace4c9838fa67b241edb65410253336358c2a
joancele <joseceballos.developer@gmail.com>
2022-10-05 02:20:15 +0200

    lesson 1

---
+++ index.js
@@ -0,0 +1,29 @@
+let firstCard = Math.floor(Math.random() * 10) + 1
+let secondCard = Math.floor(Math.random() * 10) + 1
+
+let sum = firstCard + secondCard
+
+let hasBlackJack = false
+let isAlive = true
+
+let message = ""
+
+console.log(sum)
+
+function checkBlackJack ()
+{
+    if (sum <= 20)
+    {
+
+    }
+
+    else if (sum === 21)
+    {
+        hasBlackJack = true
+    }
+
+    else
+    {
+        isAlive = false
+    }
+}
